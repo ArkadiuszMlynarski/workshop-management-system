879cd0ae17e3ad34fa0476fc97e15c0b5162d836
arkadiuszmlynarski <piromysl@gmail.com>
2019-04-10 20:30:20 +0200

    create project

---
+++ src/main/java/com/msi/CarsMechanic/CarsMechanic/CarsMechanicApplication.java
@@ -0,0 +1,13 @@
+package com.msi.CarsMechanic.CarsMechanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsMechanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsMechanicApplication.class, args);
+	}
+
+}
